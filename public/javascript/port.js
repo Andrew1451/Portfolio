f53b6328ab87f4f49b4678f63ae68b32d8a1ec2d
Andrew <andrew.gaynor@ymail.com>
2021-07-20 12:44:52 -0400

    add animation to show projects

--- public/javascript/port.js
+++ public/javascript/port.js
@@ -1,4 +1,13 @@
-// var res = document.querySelector(".resume");
-// window.addEventListener("load", function() {
-//     res.classList.add("resume2");
-// });
+const images = document.querySelectorAll('.images')
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.target.classList.contains('leftimages')) {
+            entry.intersectionRatio > 0 ? entry.target.style.animation = 'fromLeft 1s forwards ease-out' : entry.target.style.animation = 'none'
+        }
+        if (entry.target.classList.contains('rightimages')) {
+            entry.intersectionRatio > 0 ? entry.target.style.animation = 'fromRight 1s forwards ease-out' : entry.target.style.animation = 'none'
+        }
+    })
+})
+images.forEach(image => observer.observe(image))
