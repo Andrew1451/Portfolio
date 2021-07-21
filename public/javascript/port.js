eff383b9bec2fe87d4f4032de14bb9f4eeae08d1
Andrew <andrew.gaynor@ymail.com>
2021-07-21 12:37:01 -0400

    touch up animations

--- public/javascript/port.js
+++ public/javascript/port.js
@@ -1,13 +1,19 @@
 const images = document.querySelectorAll('.images')
 
+const options = {
+    root: null,
+    rootMargin: '-100px',
+    threshold: []
+}
+
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.target.classList.contains('leftimages')) {
-            entry.intersectionRatio > 0 ? entry.target.style.animation = 'fromLeft 1s forwards ease-out' : entry.target.style.animation = 'none'
+            entry.intersectionRatio > 0 ? entry.target.style.animation = 'fromLeft 1s forwards ease-out' : entry.target.style.animation = 'toLeft 1s forwards ease-out'
         }
         if (entry.target.classList.contains('rightimages')) {
-            entry.intersectionRatio > 0 ? entry.target.style.animation = 'fromRight 1s forwards ease-out' : entry.target.style.animation = 'none'
+            entry.intersectionRatio > 0 ? entry.target.style.animation = 'fromRight 1s forwards ease-out' : entry.target.style.animation = 'toRight 1s forwards ease-out'
         }
     })
-})
+}, options)
 images.forEach(image => observer.observe(image))
